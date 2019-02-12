e8840be6285ede18d7550041971446c8edc04b36
Jim Crist <jiminy.crist@gmail.com>
2019-02-12 13:57:40 -0600

    Add runtime configuration

--- main.go
+++ main.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"io"
 	"net"
+	"net/http"
+	"os"
+	"strings"
 	"sync"
 	"time"
 )
 
 var (
 	address        string
+	apiAddress     string
 	timeout        time.Duration
 	logLevelString string
 )
 
 type Proxy struct {
-	address string
-	timeout time.Duration
-	logger  *Logger
+	address       string
+	apiAddress    string
+	configAddress string
+	timeout       time.Duration
+	logger        *Logger
+	token         string
+	routes        map[string]string
+	routesLock    sync.RWMutex
 }
 
 type Conn struct {
@@ -29,6 +39,10 @@ type Conn struct {
 	tlsMinor int
 }
 
+type RouteMsg struct {
+	Target string `json:"target"`
+}
+
 type tlsAlert int8
 
 const (
@@ -36,9 +50,87 @@ const (
 	unrecognizedName          = 112
 )
 
-func NewProxy(address string, timeout time.Duration, logLevel LogLevel) *Proxy {
-	logger := NewLogger(logLevel)
-	return &Proxy{address: address, timeout: timeout, logger: logger}
+func NewProxy(address string, apiAddress string, token string, timeout time.Duration, logLevel LogLevel) *Proxy {
+	return &Proxy{
+		address:    address,
+		apiAddress: apiAddress,
+		timeout:    timeout,
+		logger:     NewLogger(logLevel),
+		token:      token,
+		routes:     make(map[string]string),
+	}
+}
+
+func (p *Proxy) isAuthorized(r *http.Request) bool {
+	auth := r.Header.Get("Authorization")
+	if auth == "" {
+		return false
+	}
+	parts := strings.SplitN(auth, " ", 2)
+	if len(parts) != 2 || parts[0] != "token" || parts[1] != p.token {
+		return false
+	}
+	return true
+}
+
+func (p *Proxy) routesHandler(w http.ResponseWriter, r *http.Request) {
+	if !p.isAuthorized(r) {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	route := r.URL.Path[len("/api/routes/"):]
+	if route == "" {
+		switch r.Method {
+		case http.MethodGet:
+			p.routesLock.RLock()
+			js, err := json.Marshal(p.routes)
+			p.routesLock.RUnlock()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	} else {
+		switch r.Method {
+		case http.MethodPut:
+			if r.Body == nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+			decoder := json.NewDecoder(r.Body)
+			var msg RouteMsg
+			err := decoder.Decode(&msg)
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+			}
+			p.routesLock.Lock()
+			p.routes[route] = msg.Target
+			p.routesLock.Unlock()
+			w.WriteHeader(http.StatusNoContent)
+		case http.MethodDelete:
+			p.routesLock.Lock()
+			delete(p.routes, route)
+			p.routesLock.Unlock()
+			w.WriteHeader(http.StatusNoContent)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func (p *Proxy) serveAPI() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/routes/", p.routesHandler)
+
+	server := &http.Server{
+		Addr:    p.apiAddress,
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
 
 func (p *Proxy) Run() {
@@ -47,7 +139,9 @@ func (p *Proxy) Run() {
 		p.logger.Errorf("Failed to connect: %s", err)
 		return
 	}
-	p.logger.Infof("Serving at %s", p.address)
+	p.logger.Infof("Proxy serving at %s", p.address)
+	p.logger.Infof("API serving at %s", p.apiAddress)
+	go p.serveAPI()
 	for {
 		c, err := l.Accept()
 		if err != nil {
@@ -74,10 +168,6 @@ func (p *Proxy) sendAlert(c *Conn, alert tlsAlert, format string, args ...interf
 	}
 }
 
-func (p *Proxy) match(sni string) string {
-	return "172.217.11.46:443"
-}
-
 func (p *Proxy) proxy(c *Conn) {
 	defer c.inConn.Close()
 
@@ -101,7 +191,7 @@ func (p *Proxy) proxy(c *Conn) {
 		return
 	}
 
-	c.outAddr = p.match(c.sni)
+	c.outAddr = p.routes[c.sni]
 	if c.outAddr == "" {
 		p.sendAlert(c, unrecognizedName, "No destination found for %q", c.sni)
 		return
@@ -139,14 +229,19 @@ func (p *Proxy) proxyConnections(wg *sync.WaitGroup, in, out *net.TCPConn) {
 }
 
 func init() {
-	flag.StringVar(&address, "address", ":443", "Listening address")
+	flag.StringVar(&address, "address", ":8786", "Proxy listening address")
+	flag.StringVar(&apiAddress, "api-address", ":8787", "API listening address")
 	flag.DurationVar(&timeout, "timeout", 3*time.Second, "Timeout for TLS Handshake initialization")
 	flag.StringVar(&logLevelString, "log-level", "INFO", "The log level. One of {DEBUG, INFO, WARN, ERROR}")
 }
 
 func main() {
 	flag.Parse()
+	token := os.Getenv("CONFIG_SNI_PROXY_TOKEN")
+	if token == "" {
+		panic("CONFIG_SNI_PROXY_TOKEN environment variable not set")
+	}
 	logLevel := ParseLevel(logLevelString)
-	p := NewProxy(address, timeout, logLevel)
+	p := NewProxy(address, apiAddress, token, timeout, logLevel)
 	p.Run()
 }
