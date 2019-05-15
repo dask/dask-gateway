ee7605766a7ed402dd4b2eca0a2b908bb3c7d0ae
Jim Crist <jiminy.crist@gmail.com>
2019-05-15 17:05:28 -0500

    Add travis support

---
+++ continuous_integration/install.sh
@@ -0,0 +1,27 @@
+set -xe
+
+python3.7 -m venv test-environment
+
+source test-environment/bin/activate
+
+pip install \
+    dask \
+    distributed \
+    cryptography \
+    tornado \
+    traitlets \
+    sqlalchemy \
+    pytest \
+    pytest-asyncio \
+    black \
+    flake8
+
+pushd dask-gateway
+python setup.py develop
+popd
+
+pushd dask-gateway-server
+python setup.py develop
+popd
+
+pip list
