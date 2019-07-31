b235bfeef0aa003e200a8cced7d28617e4c72d17
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-07-31 18:34:38 +0000

    Update docs after building Travis build 121 of jcrist/dask-gateway
    The docs were built from the branch 'master' against the commit
    2ec7d1cb01ea0a4eefa0f37077b2fe291a8daa3e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/dask-gateway/jobs/566115807.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy . --built-docs docs/build/html/

--- _sources/api-client.rst.txt
+++ _sources/api-client.rst.txt
@@ -22,3 +22,11 @@ Options
 -------
 
 .. autoclass:: dask_gateway.options.Options
+
+
+Exceptions
+----------
+
+.. autoclass:: dask_gateway.GatewayClusterError
+
+.. autoclass:: dask_gateway.GatewayServerError
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+5.g03242fb',
+    VERSION: '0.2.0+7.g2ec7d1c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
