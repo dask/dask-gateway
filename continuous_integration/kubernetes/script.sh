74c08ded5d256f291186dd816bdbbd3f2076f8dc
Joseph Hamman <jhamman@ucar.edu>
2019-10-07 13:50:46 -0700

    build base image in script.sh [test-kube]

--- continuous_integration/kubernetes/script.sh
+++ continuous_integration/kubernetes/script.sh
@@ -5,6 +5,7 @@ kubectl exec dask-gateway-tests -n dask-gateway /working/continuous_integration/
 
 echo "Running chartpress"
 pushd $TRAVIS_BUILD_DIR/resources/helm/
+docker build -t dask-gateway-base images/dask-gateway-base
 chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --tag `date +%y.%m.%d`
 git diff
 
