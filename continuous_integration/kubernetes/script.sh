d3e2c2d76a2cd83d45fc4042482fc5176ed005f5
Joseph Hamman <jhamman@ucar.edu>
2019-10-07 13:30:21 -0700

    pushd to chart dir [test-kube]

--- continuous_integration/kubernetes/script.sh
+++ continuous_integration/kubernetes/script.sh
@@ -4,9 +4,11 @@ echo "Running tests..."
 kubectl exec dask-gateway-tests -n dask-gateway /working/continuous_integration/kubernetes/script-internal.sh
 
 echo "Running chartpress"
+pushd $TRAVIS_BUILD_DIR/resources/helm/
 chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --tag `date +%y.%m.%d`
 git diff
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
     ./continuous_integration/kubernetes/deploy.sh
 fi
+popd
