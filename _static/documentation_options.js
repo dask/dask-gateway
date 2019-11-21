4335ef5439eec84beb14b769557370d1c8ee9c57
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-11-21 16:35:56 +0000

    Update docs after building Travis build 233 of dask/dask-gateway
    The docs were built from the branch 'master' against the commit
    441a458b324056a95788f43f799d3bac1c148d0d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/dask/dask-gateway/jobs/615144455.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0+5.ga6dd6bb',
+    VERSION: '0.5.0+6.g441a458',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
