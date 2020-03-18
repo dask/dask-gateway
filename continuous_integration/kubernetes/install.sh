9003c7eff1b6da09700b5b42bfad7058c2211f42
Jim Crist <jiminy.crist@gmail.com>
2020-03-18 17:09:02 -0500

    Install dask-gateway-server as well

--- continuous_integration/kubernetes/install.sh
+++ continuous_integration/kubernetes/install.sh
@@ -3,14 +3,22 @@ set -xe
 
 pip install \
     aiohttp \
+    black \
+    colorlog \
+    cryptography \
     dask \
     distributed \
     flake8 \
     pytest \
     pytest-asyncio \
-    kubernetes_asyncio
+    kubernetes-asyncio \
+    traitlets
 
 pushd dask-gateway
+python setup.py develop
+popd
+
+pushd dask-gateway-server
 python setup.py develop
 popd
 
