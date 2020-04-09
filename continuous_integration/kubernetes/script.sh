78b0e1116fc2e481f41d00b813b58644f0c81310
Jim Crist-Harif <jcrist@users.noreply.github.com>
2020-04-09 11:18:18 -0500

    Fix k8s helm chart support for prefixed deployments  (#241)
    * Fix k8s helm chart support for prefixed deployments
    
    Fixes support for deploying dask-gateway on k8s with all routes served
    under a prefix (e.g. `/services/dask-gateway/`).  This was woefully
    broken on master, and due to lack of tests was missed.

--- continuous_integration/kubernetes/script.sh
+++ continuous_integration/kubernetes/script.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-TEST_DASK_GATEWAY_KUBE=true TEST_DASK_GATEWAY_KUBE_ADDRESS=http://localhost:30200 py.test tests/kubernetes/ -vv
+TEST_DASK_GATEWAY_KUBE=true TEST_DASK_GATEWAY_KUBE_ADDRESS=http://localhost:30200/services/dask-gateway/ py.test tests/kubernetes/ -vv
