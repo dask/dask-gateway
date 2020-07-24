97a8bfcb50f6db2b757740c0fb1ca9af78054769
Jim Crist-Harif <jcristharif@gmail.com>
2020-07-24 10:22:27 -0500

    Bump go version in ci

--- continuous_integration/before_install.sh
+++ continuous_integration/before_install.sh
@@ -1,5 +1,5 @@
-gimme 1.12 \
-&& source ~/.gimme/envs/go1.12.env \
+gimme 1.14 \
+&& source ~/.gimme/envs/go1.14.env \
 && go get github.com/stretchr/testify/assert \
 && nvm install 6 \
 && nvm use 6
