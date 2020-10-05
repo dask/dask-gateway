515b4fb9734cfde5182f5eb4b29386c073da3167
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-10-05 09:24:13 -0500

    dry-run test deployment

--- continuous_integration/kubernetes/script.sh
+++ continuous_integration/kubernetes/script.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 TEST_DASK_GATEWAY_KUBE=true TEST_DASK_GATEWAY_KUBE_ADDRESS=http://localhost:30200/services/dask-gateway/ py.test tests/kubernetes/ -vv
+
+helm install --dry-run --debug --generate-name resources/helm/dask-gateway/ -f continuous_integration/kubernetes/test_config.yaml
