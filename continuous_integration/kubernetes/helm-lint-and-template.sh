dbad6126f714dc32895167923bf855c5faf2d4e7
Erik Sundell <erik.i.sundell@gmail.com>
2021-08-25 15:28:12 +0200

    Cleanup outdated travis references

--- continuous_integration/kubernetes/helm-lint-and-template.sh
+++ continuous_integration/kubernetes/helm-lint-and-template.sh
@@ -7,10 +7,10 @@ git_root="$(cd "${this_dir}/../.." && pwd)"
 echo "Linting Helm Chart"
 helm lint \
     "${git_root}/resources/helm/dask-gateway" \
-    -f "${git_root}/resources/helm/testing/travis.yaml"
+    -f "${git_root}/resources/helm/testing/chart-install-values.yaml"
 
 echo "Rendering Helm Chart Templates"
 helm template dask-gateway \
     --include-crds \
     "${git_root}/resources/helm/dask-gateway" \
-    -f "${git_root}/resources/helm/testing/travis.yaml"
+    -f "${git_root}/resources/helm/testing/chart-install-values.yaml"
