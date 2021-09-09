70807396fd8f528e9b243a6bb9bae32bc958b239
Tom Augspurger <tom.w.augspurger@gmail.com>
2021-09-09 10:14:34 -0500

    Register a kubernetes pytest mark (#441)

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,8 @@ def reset_logs():
     # PDB's stdout/stderr capture can close fds that our loggers are configured
     # to write to. To prevent this, reset the log handlers before every test.
     logging.getLogger("DaskGateway").handlers.clear()
+
+
+def pytest_configure(config):
+    # Adds a marker here, rather than setup.cfg, since the repository has two packages.
+    config.addinivalue_line("markers", "kubernetes: marks a test as kubernetes-related")
