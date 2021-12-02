958435ded6578ea6c3fc79e7ffee80de87d33a38
Tom Augspurger <taugspurger@microsoft.com>
2021-12-02 12:22:24 -0600

    revert usage changes

--- dask-gateway-server/dask_gateway_server/backends/kubernetes/controller.py
+++ dask-gateway-server/dask_gateway_server/backends/kubernetes/controller.py
@@ -1052,7 +1052,7 @@ class KubeController(KubeBackendAndControllerMixin, Application):
             "--name",
             "$(DASK_GATEWAY_WORKER_NAME)",
             "--nthreads",
-            str(config.worker_threads or config.worker_cores),
+            str(config.worker_threads),
             "--memory-limit",
             str(config.worker_memory_limit),
         ]
