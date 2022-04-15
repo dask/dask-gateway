02996f637156b0e4cb67fd39f8e1a47169300c1c
Erik Sundell <erik@sundellopensource.se>
2022-04-15 21:13:56 +0200

    Add colorized headings on what commands are run

--- continuous_integration/docker/hadoop/_print_logs.sh
+++ continuous_integration/docker/hadoop/_print_logs.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-set -x
 
-printf "\n\n\n\n\n"
+# Bold high intensity green
+G='\033[1;92m'
+# No color
+NC='\033[0m'
+
+printf "\n${G}supervisorctl status${NC}\n"
 supervisorctl status
 
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/krb5kdc.log${NC}\n"
 cat /var/log/supervisor/krb5kdc.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/kadmind.log${NC}\n"
 cat /var/log/supervisor/kadmind.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/yarn-nodemanager.log${NC}\n"
 cat /var/log/supervisor/yarn-nodemanager.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/yarn-resourcemanager.log${NC}\n"
 cat /var/log/supervisor/yarn-resourcemanager.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/hdfs-namenode.log${NC}\n"
 cat /var/log/supervisor/hdfs-namenode.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/hdfs-datanode.log${NC}\n"
 cat /var/log/supervisor/hdfs-datanode.log
--- continuous_integration/docker/slurm/_print_logs.sh
+++ continuous_integration/docker/slurm/_print_logs.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-set -x
 
-printf "\n\n\n\n\n"
+# Bold high intensity green
+G='\033[1;92m'
+# No color
+NC='\033[0m'
+
+printf "\n${G}supervisorctl status${NC}\n"
 supervisorctl status
 
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisord.log${NC}\n"
 cat /var/log/supervisord.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/slurmdbd.log${NC}\n"
 cat /var/log/supervisor/slurmdbd.log
-printf "\n\n\n\n\n"
+printf "\n${G}cat /var/log/supervisor/slurmctld.log${NC}\n"
 cat /var/log/supervisor/slurmctld.log
