fbe2dd4c3fe3ee132e76fe03758f965445a7858a
Erik Sundell <erik@sundellopensource.se>
2022-04-17 21:40:19 +0200

    ci: pbs image, reduce threads and delay pip install to avoid memory peak and process termination (#536)
    * ci: pbs image, reduce threads from 4 to 2 and save memory
    
    * ci: await pbs container startup to avoid memory peaks

--- continuous_integration/docker/pbs/files/scripts/start.sh
+++ continuous_integration/docker/pbs/files/scripts/start.sh
@@ -6,10 +6,21 @@ MOM_CONF_FILE=/var/spool/pbs/mom_priv/config
 HOSTNAME=$(hostname)
 
 # Configure PBS to run all on one node
+#
+# Configuration references:
+# - https://github.com/openpbs/openpbs/blob/master/doc/man8/pbs.conf.8B
+# - https://github.com/openpbs/openpbs/blob/HEAD/doc/man8/pbs_comm.8B
+# - https://github.com/openpbs/openpbs/blob/master/doc/man8/pbs_mom.8B
+#
 sed -i "s/PBS_SERVER=.*/PBS_SERVER=$HOSTNAME/" $PBS_CONF_FILE
 sed -i "s/PBS_START_MOM=.*/PBS_START_MOM=1/" $PBS_CONF_FILE
 sed -i "s/\$clienthost .*/\$clienthost $HOSTNAME/" $MOM_CONF_FILE
 echo "\$usecp *:/ /" >> $MOM_CONF_FILE
+
+# Reduce the memory footprint by using less threads to avoid the OOMKiller in
+# GitHub Actions as observed with exit code 137.
+#
+echo "PBS_COMM_THREADS=2" >> $PBS_CONF_FILE
 
 # Start PBS
 /etc/init.d/pbs start
@@ -21,4 +32,9 @@ echo "\$usecp *:/ /" >> $MOM_CONF_FILE
 /opt/pbs/bin/qmgr -c "set node pbs queue=workq"
 /opt/pbs/bin/qmgr -c "set server operators += dask@pbs"
 
+# "Entering sleep" can be used as a signal in logs that we have passed the
+# initialization phase where the memory needs may peak and expose us to the
+# OOMKiller and 137 exit codes.
+#
+echo "Entering sleep"
 sleep infinity
--- continuous_integration/docker/pbs/start.sh
+++ continuous_integration/docker/pbs/start.sh
@@ -14,3 +14,23 @@ docker run --rm -d \
     -p 8088:8088 \
     --cap-add=SYS_RESOURCE \
     ghcr.io/dask/dask-gateway-ci-pbs
+
+# The pbs container's entrypoint, files/scripts/start.sh, emits a log message
+# that we will await observing. We do it to avoid getting OOMKilled by peaking
+# memory needs during startup, which is prone to happen if we run pip install at
+# the same time.
+#
+set +x
+await_startup() {
+    i=0; while [ $i -ne 30 ]; do
+        docker logs pbs 2>/dev/null | grep --silent "Entering sleep" \
+            && start_script_finishing=true && break \
+            || start_script_finishing=false && sleep 1 && i=$((i + 1)) && echo "Waiting for pbs container startup ($i seconds)"
+    done
+    if [ "$start_script_finishing" != "true" ]; then
+        echo "WARNING: /script/start.sh was slow to finish!"
+        exit 1
+    fi
+    echo "pbs container started!"
+}
+await_startup
