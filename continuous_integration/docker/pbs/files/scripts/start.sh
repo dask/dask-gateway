0abd244c55c1fbc9a12c34e66cf732e70427251b
Erik Sundell <erik@sundellopensource.se>
2022-04-17 19:19:22 +0200

    ci: pbs image, reduce threads from 4 to 2 and save memory

--- continuous_integration/docker/pbs/files/scripts/start.sh
+++ continuous_integration/docker/pbs/files/scripts/start.sh
@@ -6,10 +6,21 @@ MOM_CONF_FILE=/var/spool/pbs/mom_priv/config
 HOSTNAME=$(hostname)
 
 # Configure PBS to run all on one node
+#
+# Configuration references:
+# - https://github.com/openpbs/openpbs/blob/master/doc/man8/pbs.conf.8B
+# - https://github.com/openpbs/openpbs/blob/HEAD/doc/man8/pbs_comm.8B
+# - https://github.com/openpbs/openpbs/blob/master/doc/man8/pbs_mom.8B
+#
 sed -i "s/PBS_SERVER=.*/PBS_SERVER=$HOSTNAME/" $PBS_CONF_FILE
 sed -i "s/PBS_START_MOM=.*/PBS_START_MOM=1/" $PBS_CONF_FILE
 sed -i "s/\$clienthost .*/\$clienthost $HOSTNAME/" $MOM_CONF_FILE
 echo "\$usecp *:/ /" >> $MOM_CONF_FILE
+
+# Reduce the memory footprint by using less threads to avoid the OOMKiller in
+# GitHub Actions as observed with exit code 137.
+#
+echo "PBS_COMM_THREADS=2" >> $PBS_CONF_FILE
 
 # Start PBS
 /etc/init.d/pbs start
