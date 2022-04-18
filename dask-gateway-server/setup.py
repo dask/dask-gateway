7fcaa21507615f475ca9720cf40b26e348782164
Erik Sundell <erik@sundellopensource.se>
2022-04-18 18:32:48 +0200

    maint: let setup.py tag dask-gateway-server wheels for pypi

--- dask-gateway-server/setup.py
+++ dask-gateway-server/setup.py
@@ -1,7 +1,20 @@
+"""
+dask-gateway-server has Golang source code in ./dask-gateway-proxy that we want
+to compile as part of installing this Package and building a source distribution
+or wheels. There is also metadata of a wheel that we would like to set. Because
+of this, this setup.py file includes some related complexity.
+
+Relevant reference documentation:
+- https://setuptools.pypa.io/en/latest/userguide/extension.html#adding-commands
+
+"""
 import errno
 import os
 import subprocess
 import sys
+
+# distutils is deprecated but we need to keep using these imports, see
+# https://github.com/pypa/setuptools/issues/2928
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
 
@@ -14,35 +27,48 @@ PROXY_SRC_DIR = os.path.join(ROOT_DIR, "dask-gateway-proxy")
 PROXY_TGT_DIR = os.path.join(ROOT_DIR, "dask_gateway_server", "proxy")
 PROXY_TGT_EXE = os.path.join(PROXY_TGT_DIR, "dask-gateway-proxy")
 
+# determine version from _version.py
 ns = {}
 with open(os.path.join(ROOT_DIR, "dask_gateway_server", "_version.py")) as f:
     exec(f.read(), {}, ns)
     VERSION = ns["__version__"]
 
-NO_PROXY = "--no-build-proxy" in sys.argv
-if NO_PROXY:
-    package_data = {}
-else:
-    package_data = {"dask_gateway_server": ["proxy/dask-gateway-proxy"]}
+
+# FIXME: Does it make sense to put the binary next to the source code
+#        (proxy/dask-gateway-proxy)? If we build a wheel now we get a folder in
+#        the wheel called purelib that includes this and it seems contradictory.
+#
+#        An action point to resolve this would be to investigate how other
+#        projects have bundled compiled binaries.
+#
+package_data = {"dask_gateway_server": ["proxy/dask-gateway-proxy"]}
 
 
 class build_proxy(Command):
-    description = "build go artifacts"
+    """
+    A command the compile the golang code in ./dask-gateway-proxy and copy the
+    executable binary to dask_gateway_server/proxy/dask-gateway-proxy to be
+    picked up via package_data.
 
+    setuptools.Command derived classes are required to implement
+    initialize_options, finalize_options, and run.
+    """
+
+    description = "build go artifact"
     user_options = []
 
     def initialize_options(self):
         pass
 
-    finalize_options = initialize_options
+    def finalize_options(self):
+        pass
 
     def run(self):
-        # Compile the go code and copy the executable to dask_gateway_server/proxy/
-        # This will be picked up as package_data later
         self.mkpath(PROXY_TGT_DIR)
         try:
             code = subprocess.call(
-                ["go", "build", "-o", PROXY_TGT_EXE], cwd=PROXY_SRC_DIR
+                ["go", "build", "-o", PROXY_TGT_EXE],
+                cwd=PROXY_SRC_DIR,
             )
         except OSError as exc:
             if exc.errno == errno.ENOENT:
@@ -50,7 +76,7 @@ class build_proxy(Command):
                     "Building dask-gateway-server requires a go compiler, "
                     "which wasn't found in your environment. Please install "
                     "go using a package manager (e.g. apt, brew, conda, etc...), "
-                    "or download from https://golang.org/dl/."
+                    "or download from https://go.dev/dl/."
                 )
                 sys.exit(1)
             raise
@@ -58,44 +84,127 @@ class build_proxy(Command):
             sys.exit(code)
 
 
-class mixin:
+class build_proxy_mixin:
+    """
+    _no_build_proxy_sticky retains memory if this flag has been passed. This is
+    needed as running "python setup.py install" leads to both install and build
+    commands are executed but only the install command is passed the flag.
+    """
+
+    _no_build_proxy_option = (
+        "no-build-proxy",
+        None,
+        "Don't build the dask-gateway-proxy executable from its Golang source code",
+    )
+    _no_build_proxy_sticky = "--no-build-proxy" in sys.argv
+
     def initialize_options(self):
-        self.no_build_proxy = False
         super().initialize_options()
+        self.no_build_proxy = False
+
+    def finalize_options(self):
+        super().finalize_options()
+        if self.no_build_proxy:
+            __class__._no_build_proxy_sticky = True
 
     def run(self):
-        global NO_PROXY
-        if self.no_build_proxy:
-            NO_PROXY = True
-        if not getattr(self, "uninstall", False):
-            if not NO_PROXY and not os.path.exists(PROXY_TGT_EXE):
-                self.run_command("build_proxy")
+        if (
+            not __class__._no_build_proxy_sticky
+            and not getattr(self, "uninstall", False)
+            and not os.path.exists(PROXY_TGT_EXE)
+        ):
+            self.run_command("build_proxy")
         super().run()
 
 
-class build(mixin, _build):
+if build_proxy_mixin._no_build_proxy_sticky:
+    package_data = {}
+else:
+    package_data = {"dask_gateway_server": ["proxy/dask-gateway-proxy"]}
+
+
+class build(build_proxy_mixin, _build):
     user_options = list(_build.user_options)
-    user_options.append(("no-build-proxy", None, "Don't build the proxy source"))
+    user_options.append(build_proxy_mixin._no_build_proxy_option)
 
 
-class install(mixin, _install):
+class install(build_proxy_mixin, _install):
     user_options = list(_install.user_options)
-    user_options.append(("no-build-proxy", None, "Don't build the proxy source"))
+    user_options.append(build_proxy_mixin._no_build_proxy_option)
 
 
-class develop(mixin, _develop):
+class develop(build_proxy_mixin, _develop):
     user_options = list(_develop.user_options)
-    user_options.append(("no-build-proxy", None, "Don't build the proxy source"))
+    user_options.append(build_proxy_mixin._no_build_proxy_option)
 
 
 class clean(_clean):
     def run(self):
         if self.all:
-            for f in [PROXY_TGT_EXE]:
-                if os.path.exists(f):
-                    os.unlink(f)
+            if os.path.exists(PROXY_TGT_EXE):
+                os.remove(PROXY_TGT_EXE)
         _clean.run(self)
 
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(build_proxy_mixin, _bdist_wheel):
+        user_options = list(_bdist_wheel.user_options)
+        user_options.append(build_proxy_mixin._no_build_proxy_option)
+
+        def finalize_options(self):
+            super().finalize_options()
+            self.root_is_pure = False
+
+        def get_tag(self):
+            """
+            We build a Python package with a platform dependency as indicated by
+            setting root_is_pure to False, but the `get_tag` implementation
+            assumes it relates to "cpython abi components" which is incorrect.
+            The platform dependency is a Golang compiled executable binary
+            specific to the operating system and CPU architecture.
+
+            This adjustment was based on
+            https://stackoverflow.com/a/45150383/2220152.
+            """
+            python, abi, plat = super().get_tag()
+            python, abi = "py3", "none"
+
+            # If GOOS or GOARCH are set, we are probably cross compiling. For
+            # some limited know configurations, lets support building the wheel
+            # with cross compiling.
+            #
+            # PyPI restricts us to using the following platform tags:
+            # https://github.com/pypa/warehouse/blob/82815b06d9f98deed5f205c66e054de59d22a10d/warehouse/forklift/legacy.py#L108-L180
+            #
+            # For reference, GitHub Actions provides workers with the following
+            # operating systems:
+            # https://github.com/actions/virtual-environments#available-environments
+            #
+            go_plat = os.environ.get("GOOS", "") + "_" + os.environ.get("GOARCH", "")
+            go_plat_to_pypi_plat_mapping = {
+                "linux_amd64": "manylinux2014_x86_64",
+                "linux_arm64": "manylinux2014_aarch64",
+                "darwin_amd64": "macosx_10_15_x86_64",
+                "darwin_arm64": "macosx_11_0_arm64",
+            }
+            if go_plat in go_plat_to_pypi_plat_mapping:
+                plat = go_plat_to_pypi_plat_mapping[go_plat]
+
+            return python, abi, plat
+
+except ImportError:
+    bdist_wheel = None
+
+cmdclass = {
+    "bdist_wheel": bdist_wheel,  # update built wheel descriptions
+    "build_proxy": build_proxy,  # directly build the proxy source
+    "build": build,  # bdist_wheel or pip install .
+    "install": install,  # python setup.py install
+    "develop": develop,  # python setup.py develop
+    "clean": clean,
+}
 
 with open("requirements.txt") as f:
     install_requires = [l for l in f.readlines() if not l.startswith("#")]
@@ -115,18 +224,10 @@ extras_require = {
     ],
 }
 
-# Due to quirks in setuptools/distutils dependency ordering, to get the go
-# source to build automatically in most cases, we need to check in multiple
-# locations. This is unfortunate, but seems necessary.
-cmdclass = {
-    "build_proxy": build_proxy,  # directly build the proxy source
-    "build": build,  # bdist_wheel or pip install .
-    "install": install,  # python setup.py install
-    "develop": develop,  # python setup.py develop
-    "clean": clean,
-}
 
-
+# setup's keyword reference:
+# https://setuptools.pypa.io/en/latest/references/keywords.html
+#
 setup(
     name="dask-gateway-server",
     version=VERSION,
