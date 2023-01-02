9d438932d0773285807867ccdbe8c538a843cb51
Erik Sundell <erik.i.sundell@gmail.com>
2023-01-02 17:06:38 +0100

    ci: workaround failure to install pyzmq with pip

--- continuous_integration/docker/pbs/_install.sh
+++ continuous_integration/docker/pbs/_install.sh
@@ -3,6 +3,12 @@ set -xe
 
 cd /working
 
+# FIXME: Don't install pyzmq from conda-forge when pyzmq 25.0.0 is released, see
+#        https://github.com/zeromq/pyzmq/issues/1821 and
+#        https://pypi.org/project/pyzmq/.
+#
+mamba install -c conda-forge pyzmq
+
 # This installs everything we need for tests
 pip install -r tests/requirements.txt
 
