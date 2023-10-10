95f640225ae8af8a0b05902f75edbab03361e97f
mike dupont <mike.dupont@introspector.local>
2023-10-10 14:32:04 -0400

    pector -> spector

--- continuous_integration/docker/build.sh
+++ continuous_integration/docker/build.sh
@@ -1,4 +1,12 @@
-docker build --no-cache -t ghcr.io/meta-introspector/dask-gateway-ci-base ./base
-docker build --no-cache -t ghcr.io/meta-introspector/dask-gateway-ci-hadoop ./hadoop
-docker build --no-cache -t ghcr.io/meta-introspector/dask-gateway-ci-pbs ./pbs
-docker build --no-cache -t ghcr.io/meta-introspector/dask-gateway-ci-slurm ./slurm
+
+# --no-cache
+docker build  -t ghcr.io/meta-introspector/dask-gateway-ci-base ./base --label "org.opencontainers.image.source=https://github.com/meta-introspector/dask-gateway"      --label "org.opencontainers.image.description=Test image" 
+
+docker build  -t ghcr.io/meta-introspector/dask-gateway-ci-hadoop ./hadoop --label "org.opencontainers.image.source=https://github.com/meta-introspector/dask-gateway"      --label "org.opencontainers.image.description=Test image" 
+docker build  -t ghcr.io/meta-introspector/dask-gateway-ci-pbs ./pbs 
+docker build  -t ghcr.io/meta-introspector/dask-gateway-ci-slurm ./slurm --label "org.opencontainers.image.source=https://github.com/meta-introspector/dask-gateway"      --label "org.opencontainers.image.description=Test image" 
+
+
+
+# 
+# --
