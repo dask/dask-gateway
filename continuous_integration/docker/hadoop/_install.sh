3252e611008fd8d334a449da79819cf9df7316db
Erik Sundell <erik.i.sundell@gmail.com>
2025-01-20 12:57:29 +0100

    ci: remove workaround installing pymzq via conda-forge

--- continuous_integration/docker/hadoop/_install.sh
+++ continuous_integration/docker/hadoop/_install.sh
@@ -6,11 +6,7 @@ cd /working
 # pykerberos needs to compile c++ code that depends on system libraries, by
 # installing it from conda-forge, we avoid such hassle.
 #
-# FIXME: Don't install pyzmq from conda-forge when pyzmq 25.0.0 is released, see
-#        https://github.com/zeromq/pyzmq/issues/1821 and
-#        https://pypi.org/project/pyzmq/.
-#
-mamba install -c conda-forge pykerberos pyzmq
+mamba install -c conda-forge pykerberos
 
 # This installs everything else we need for tests
 pip install -r tests/requirements.txt
