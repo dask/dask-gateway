b64366426f70227cbe79513bcf6db59c87cf3525
consideRatio <erik.i.sundell@gmail.com>
2025-04-10 14:50:28 +0000

    Deploying to gh-pages from @ dask/dask-gateway@6f2ad287d7a65238eec86f31af7675032ee0b796 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2025.4.0',
+    VERSION: '2025.4.1-0.dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
