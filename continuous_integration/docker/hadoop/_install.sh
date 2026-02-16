9ac1f4c3f8f6685befd94b19fb538e464a2817ac
Erik Sundell <erik.i.sundell@gmail.com>
2026-02-16 09:16:05 +0100

    ci: bump pip, install contourpy/pandas/greenlet via conda-forge (#926)

--- continuous_integration/docker/hadoop/_install.sh
+++ continuous_integration/docker/hadoop/_install.sh
@@ -6,7 +6,7 @@ cd /working
 # FIXME: pip should be installed to a modern version in the base image instead
 #        of being upgraded here. It isn't because of
 #        https://github.com/dask/dask-gateway/issues/837.
-pip install "pip==24.*"
+pip install "pip==26.*"
 
 # pykerberos needs to compile c++ code that depends on system libraries, by
 # installing it from conda-forge, we avoid such hassle.
