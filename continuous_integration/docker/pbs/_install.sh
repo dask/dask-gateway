3bdc02ed449c198bdc36915dec31e4e2b70b34cb
Erik Sundell <erik.sundell+2025@sensmetry.com>
2026-02-16 08:50:24 +0100

    ci: bump pip, install contourpy/pandas/greenlet via conda-forge

--- continuous_integration/docker/pbs/_install.sh
+++ continuous_integration/docker/pbs/_install.sh
@@ -6,7 +6,12 @@ cd /working
 # FIXME: pip should be installed to a modern version in the base image instead
 #        of being upgraded here. It isn't because of
 #        https://github.com/dask/dask-gateway/issues/837.
-pip install "pip==24.*"
+pip install "pip==26.*"
+
+# some packages needs to compile c++ code that depends on system libraries, by
+# installing it from conda-forge, we avoid such hassle.
+#
+mamba install -c conda-forge contourpy pandas greenlet
 
 # This installs everything we need for tests
 pip install -r tests/requirements.txt
